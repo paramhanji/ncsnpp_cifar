471f451386cfe3fbc4b83cc2d5024547726c59c9
Param Hanji <param.catchchaos@gmail.com>
2023-10-27 01:44:32 +0100

    Create ncsnpp.py

---
+++ ncsnpp.py
@@ -0,0 +1,405 @@
+import functools
+import math
+import numpy as np
+import string
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import up_or_down_sampling
+
+
+def conv3x3(in_planes, out_planes, stride=1, bias=True, dilation=1, init_scale=1., padding=1):
+  """3x3 convolution with DDPM initialization."""
+  conv = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=padding,
+                   dilation=dilation, bias=bias)
+  conv.weight.data = variance_scaling(init_scale)(conv.weight.data.shape)
+  nn.init.zeros_(conv.bias)
+  return conv
+
+def conv1x1(in_planes, out_planes, stride=1, bias=True, init_scale=1., padding=0):
+  """1x1 convolution with DDPM initialization."""
+  conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=padding, bias=bias)
+  conv.weight.data = variance_scaling(init_scale)(conv.weight.data.shape)
+  nn.init.zeros_(conv.bias)
+  return conv
+
+def get_timestep_embedding(timesteps, embedding_dim, max_positions=10000):
+  assert len(timesteps.shape) == 1  # and timesteps.dtype == tf.int32
+  half_dim = embedding_dim // 2
+  # magic number 10000 is from transformers
+  emb = math.log(max_positions) / (half_dim - 1)
+  # emb = math.log(2.) / (half_dim - 1)
+  emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
+  # emb = tf.range(num_embeddings, dtype=jnp.float32)[:, None] * emb[None, :]
+  # emb = tf.cast(timesteps, dtype=jnp.float32)[:, None] * emb[None, :]
+  emb = timesteps.float()[:, None] * emb[None, :]
+  emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
+  if embedding_dim % 2 == 1:  # zero pad
+    emb = F.pad(emb, (0, 1), mode='constant')
+  assert emb.shape == (timesteps.shape[0], embedding_dim)
+  return emb
+
+def variance_scaling(scale=1.,
+                     in_axis=1, out_axis=0,
+                     dtype=torch.float32,
+                     device='cpu'):
+
+  def _compute_fans(shape, in_axis=1, out_axis=0):
+    receptive_field_size = np.prod(shape) / shape[in_axis] / shape[out_axis]
+    fan_in = shape[in_axis] * receptive_field_size
+    fan_out = shape[out_axis] * receptive_field_size
+    return fan_in, fan_out
+
+  def init(shape, dtype=dtype, device=device):
+    fan_in, fan_out = _compute_fans(shape, in_axis, out_axis)
+    denominator = (fan_in + fan_out) / 2
+    variance = scale / denominator
+    return (torch.rand(*shape, dtype=dtype, device=device) * 2. - 1.) * np.sqrt(3 * variance)
+
+  return init
+
+
+class NCSNpp(nn.Module):
+  """NCSN++ model"""
+
+  def __init__(self, sigma_max=50, sigma_min=0.01, num_scales=1000, nf=128, ch_mult=(1, 2, 2, 2), num_res_blocks=4,
+               attn_resolutions=(16,), image_size=32, dropout=0.1, conditional=True, fir_kernel=[1, 3, 3, 1],
+               init_scale=0, channels=3):
+    super().__init__()
+    self.act = act = nn.SiLU()
+    # TODO
+    sigmas = np.exp(np.linspace(np.log(sigma_max), np.log(sigma_min), num_scales))
+    self.register_buffer('sigmas', torch.tensor(sigmas))
+
+    self.nf = nf
+    self.num_res_blocks = num_res_blocks
+    self.attn_resolutions = attn_resolutions
+    self.num_resolutions = num_resolutions = len(ch_mult)
+    all_resolutions = [image_size // (2 ** i) for i in range(num_resolutions)]
+
+    self.conditional = conditional  # noise-conditional
+    fir = fir_kernel is not None
+
+    modules = []
+
+    if conditional:
+      modules.append(nn.Linear(nf, nf * 4))
+      modules[-1].weight.data = variance_scaling()(modules[-1].weight.shape)
+      nn.init.zeros_(modules[-1].bias)
+      modules.append(nn.Linear(nf * 4, nf * 4))
+      modules[-1].weight.data = variance_scaling()(modules[-1].weight.shape)
+      nn.init.zeros_(modules[-1].bias)
+
+    AttnBlock = functools.partial(AttnBlockpp,
+                                  init_scale=init_scale,
+                                  skip_rescale=True)
+
+    pyramid_downsample = functools.partial(Downsample,
+                                            fir=fir, fir_kernel=fir_kernel, with_conv=True)
+
+    ResnetBlock = functools.partial(ResnetBlockBigGANpp,
+                                    act=act,
+                                    dropout=dropout,
+                                    fir=fir,
+                                    fir_kernel=fir_kernel,
+                                    init_scale=init_scale,
+                                    skip_rescale=True,
+                                    temb_dim=nf * 4)
+
+    # Downsampling block
+
+    input_pyramid_ch = channels
+
+    modules.append(conv3x3(channels, nf))
+    hs_c = [nf]
+
+    in_ch = nf
+    for i_level in range(num_resolutions):
+      # Residual blocks for this resolution
+      for i_block in range(num_res_blocks):
+        out_ch = nf * ch_mult[i_level]
+        modules.append(ResnetBlock(in_ch=in_ch, out_ch=out_ch))
+        in_ch = out_ch
+
+        if all_resolutions[i_level] in attn_resolutions:
+          modules.append(AttnBlock(channels=in_ch))
+        hs_c.append(in_ch)
+
+      if i_level != num_resolutions - 1:
+        modules.append(ResnetBlock(down=True, in_ch=in_ch))
+
+        modules.append(pyramid_downsample(in_ch=input_pyramid_ch, out_ch=in_ch))
+        input_pyramid_ch = in_ch
+
+        hs_c.append(in_ch)
+
+    in_ch = hs_c[-1]
+    modules.append(ResnetBlock(in_ch=in_ch))
+    modules.append(AttnBlock(channels=in_ch))
+    modules.append(ResnetBlock(in_ch=in_ch))
+
+    pyramid_ch = 0
+    # Upsampling block
+    for i_level in reversed(range(num_resolutions)):
+      for i_block in range(num_res_blocks + 1):
+        out_ch = nf * ch_mult[i_level]
+        modules.append(ResnetBlock(in_ch=in_ch + hs_c.pop(),
+                                   out_ch=out_ch))
+        in_ch = out_ch
+
+      if all_resolutions[i_level] in attn_resolutions:
+        modules.append(AttnBlock(channels=in_ch))
+
+      if i_level != 0:
+        modules.append(ResnetBlock(in_ch=in_ch, up=True))
+
+    assert not hs_c
+
+    modules.append(nn.GroupNorm(num_groups=min(in_ch // 4, 32),
+                                num_channels=in_ch, eps=1e-6))
+    modules.append(conv3x3(in_ch, channels, init_scale=init_scale))
+
+    self.all_modules = nn.ModuleList(modules)
+
+  def forward(self, x, time_cond):
+    # timestep/noise_level embedding; only for continuous training
+    modules = self.all_modules
+    m_idx = 0
+    # Sinusoidal positional embeddings.
+    timesteps = time_cond
+    used_sigmas = self.sigmas[time_cond.long()]
+    temb = get_timestep_embedding(timesteps, self.nf)
+
+    if self.conditional:
+      temb = modules[m_idx](temb)
+      m_idx += 1
+      temb = modules[m_idx](self.act(temb))
+      m_idx += 1
+    else:
+      temb = None
+
+    # Downsampling block
+    input_pyramid = x
+
+    hs = [modules[m_idx](x)]
+    m_idx += 1
+    for i_level in range(self.num_resolutions):
+      # Residual blocks for this resolution
+      for i_block in range(self.num_res_blocks):
+        h = modules[m_idx](hs[-1], temb)
+        m_idx += 1
+        if h.shape[-1] in self.attn_resolutions:
+          h = modules[m_idx](h)
+          m_idx += 1
+
+        hs.append(h)
+
+      if i_level != self.num_resolutions - 1:
+        h = modules[m_idx](hs[-1], temb)
+        m_idx += 1
+
+        input_pyramid = modules[m_idx](input_pyramid)
+        m_idx += 1
+        input_pyramid = (input_pyramid + h) / np.sqrt(2.)
+        h = input_pyramid
+
+        hs.append(h)
+
+    h = hs[-1]
+    h = modules[m_idx](h, temb)
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+    h = modules[m_idx](h, temb)
+    m_idx += 1
+
+    pyramid = None
+
+    # Upsampling block
+    for i_level in reversed(range(self.num_resolutions)):
+      for i_block in range(self.num_res_blocks + 1):
+        h = modules[m_idx](torch.cat([h, hs.pop()], dim=1), temb)
+        m_idx += 1
+
+      if h.shape[-1] in self.attn_resolutions:
+        h = modules[m_idx](h)
+        m_idx += 1
+
+      if i_level != 0:
+        h = modules[m_idx](h, temb)
+        m_idx += 1
+
+    assert not hs
+
+    h = self.act(modules[m_idx](h))
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+
+    assert m_idx == len(modules)
+
+    return h
+
+
+def _einsum(a, b, c, x, y):
+  einsum_str = '{},{}->{}'.format(''.join(a), ''.join(b), ''.join(c))
+  return torch.einsum(einsum_str, x, y)
+
+def contract_inner(x, y):
+  """tensordot(x, y, 1)."""
+  x_chars = list(string.ascii_lowercase[:len(x.shape)])
+  y_chars = list(string.ascii_lowercase[len(x.shape):len(y.shape) + len(x.shape)])
+  y_chars[0] = x_chars[-1]  # first axis of y and last of x get summed
+  out_chars = x_chars[:-1] + y_chars[1:]
+  return _einsum(x_chars, y_chars, out_chars, x, y)
+
+class NIN(nn.Module):
+  def __init__(self, in_dim, num_units, init_scale=0.1):
+    super().__init__()
+    self.W = nn.Parameter(variance_scaling(scale=init_scale)((in_dim, num_units)), requires_grad=True)
+    self.b = nn.Parameter(torch.zeros(num_units), requires_grad=True)
+
+  def forward(self, x):
+    x = x.permute(0, 2, 3, 1)
+    y = contract_inner(x, self.W) + self.b
+    return y.permute(0, 3, 1, 2)
+
+
+class AttnBlockpp(nn.Module):
+  """Channel-wise self-attention block. Modified from DDPM."""
+
+  def __init__(self, channels, skip_rescale=False, init_scale=0.):
+    super().__init__()
+    self.GroupNorm_0 = nn.GroupNorm(num_groups=min(channels // 4, 32), num_channels=channels,
+                                  eps=1e-6)
+    self.NIN_0 = NIN(channels, channels)
+    self.NIN_1 = NIN(channels, channels)
+    self.NIN_2 = NIN(channels, channels)
+    self.NIN_3 = NIN(channels, channels, init_scale=init_scale)
+    self.skip_rescale = skip_rescale
+
+  def forward(self, x):
+    B, C, H, W = x.shape
+    h = self.GroupNorm_0(x)
+    q = self.NIN_0(h)
+    k = self.NIN_1(h)
+    v = self.NIN_2(h)
+
+    w = torch.einsum('bchw,bcij->bhwij', q, k) * (int(C) ** (-0.5))
+    w = torch.reshape(w, (B, H, W, H * W))
+    w = F.softmax(w, dim=-1)
+    w = torch.reshape(w, (B, H, W, H, W))
+    h = torch.einsum('bhwij,bcij->bchw', w, v)
+    h = self.NIN_3(h)
+    if not self.skip_rescale:
+      return x + h
+    else:
+      return (x + h) / np.sqrt(2.)
+
+
+class Downsample(nn.Module):
+  def __init__(self, in_ch=None, out_ch=None, with_conv=False, fir=False,
+               fir_kernel=(1, 3, 3, 1)):
+    super().__init__()
+    out_ch = out_ch if out_ch else in_ch
+    if not fir:
+      if with_conv:
+        self.Conv_0 = conv3x3(in_ch, out_ch, stride=2, padding=0)
+    else:
+      if with_conv:
+        self.Conv2d_0 = up_or_down_sampling.Conv2d(in_ch, out_ch,
+                                                 kernel=3, down=True,
+                                                 resample_kernel=fir_kernel,
+                                                 use_bias=True,
+                                                 kernel_init=variance_scaling())
+    self.fir = fir
+    self.fir_kernel = fir_kernel
+    self.with_conv = with_conv
+    self.out_ch = out_ch
+
+  def forward(self, x):
+    B, C, H, W = x.shape
+    if not self.fir:
+      if self.with_conv:
+        x = F.pad(x, (0, 1, 0, 1))
+        x = self.Conv_0(x)
+      else:
+        x = F.avg_pool2d(x, 2, stride=2)
+    else:
+      if not self.with_conv:
+        x = up_or_down_sampling.downsample_2d(x, self.fir_kernel, factor=2)
+      else:
+        x = self.Conv2d_0(x)
+
+    return x
+
+
+class ResnetBlockBigGANpp(nn.Module):
+  def __init__(self, act, in_ch, out_ch=None, temb_dim=None, up=False, down=False,
+               dropout=0.1, fir=False, fir_kernel=(1, 3, 3, 1),
+               skip_rescale=True, init_scale=0.):
+    super().__init__()
+
+    out_ch = out_ch if out_ch else in_ch
+    self.GroupNorm_0 = nn.GroupNorm(num_groups=min(in_ch // 4, 32), num_channels=in_ch, eps=1e-6)
+    self.up = up
+    self.down = down
+    self.fir = fir
+    self.fir_kernel = fir_kernel
+
+    self.Conv_0 = conv3x3(in_ch, out_ch)
+    if temb_dim is not None:
+      self.Dense_0 = nn.Linear(temb_dim, out_ch)
+      self.Dense_0.weight.data = variance_scaling()(self.Dense_0.weight.shape)
+      nn.init.zeros_(self.Dense_0.bias)
+
+    self.GroupNorm_1 = nn.GroupNorm(num_groups=min(out_ch // 4, 32), num_channels=out_ch, eps=1e-6)
+    self.Dropout_0 = nn.Dropout(dropout)
+    self.Conv_1 = conv3x3(out_ch, out_ch, init_scale=init_scale)
+    if in_ch != out_ch or up or down:
+      self.Conv_2 = conv1x1(in_ch, out_ch)
+
+    self.skip_rescale = skip_rescale
+    self.act = act
+    self.in_ch = in_ch
+    self.out_ch = out_ch
+
+  def forward(self, x, temb=None):
+    h = self.act(self.GroupNorm_0(x))
+
+    if self.up:
+      if self.fir:
+        h = up_or_down_sampling.upsample_2d(h, self.fir_kernel, factor=2)
+        x = up_or_down_sampling.upsample_2d(x, self.fir_kernel, factor=2)
+      else:
+        h = up_or_down_sampling.naive_upsample_2d(h, factor=2)
+        x = up_or_down_sampling.naive_upsample_2d(x, factor=2)
+    elif self.down:
+      if self.fir:
+        h = up_or_down_sampling.downsample_2d(h, self.fir_kernel, factor=2)
+        x = up_or_down_sampling.downsample_2d(x, self.fir_kernel, factor=2)
+      else:
+        h = up_or_down_sampling.naive_downsample_2d(h, factor=2)
+        x = up_or_down_sampling.naive_downsample_2d(x, factor=2)
+
+    h = self.Conv_0(h)
+    # Add bias to each feature map conditioned on the time embedding
+    if temb is not None:
+      h += self.Dense_0(self.act(temb))[:, :, None, None]
+    h = self.act(self.GroupNorm_1(h))
+    h = self.Dropout_0(h)
+    h = self.Conv_1(h)
+
+    if self.in_ch != self.out_ch or self.up or self.down:
+      x = self.Conv_2(x)
+
+    if not self.skip_rescale:
+      return x + h
+    else:
+      return (x + h) / np.sqrt(2.)
+
+
+m = NCSNpp()
+o = m(torch.randn(32, 3, 32, 32), torch.rand(32))
+print(o.shape)
